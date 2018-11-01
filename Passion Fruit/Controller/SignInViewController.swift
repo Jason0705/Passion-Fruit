6230e383e78678a735cc0f3e26496146cbd3b353
Jason Li <jasonli@jasons-macbook-air.local>
2018-11-01 14:16:28 -0700

    Add Sign in page storyboard, auto layout.

---
+++ Passion Fruit/Controller/SignInViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SignInViewController.swift
+//  Passion Fruit
+//
+//  Created by Jason Li on 2018-11-01.
+//  Copyright © 2018 Jason Li. All rights reserved.
+//
+
+import UIKit
+
+class SignInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
